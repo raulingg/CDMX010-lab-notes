c0c7fcd5f310829e438c51b0c6d04e808bd09a7e
Raul Robinson Quispe Mendez <9435850+raulingg@users.noreply.github.com>
2021-05-06 16:13:06 +0000

    refactor: rehouse note list

--- src/pages/Cards.js
+++ src/pages/Cards.js
@@ -1,24 +1,50 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './Cards.css';
 import { useHistory } from 'react-router';
 import duckytwo from '../assets/img/duckytwo.png';
 import icon from '../assets/img/icon.png';
+import { db } from '../firebase';
 
 function Cards() {
-  const historyCards = useHistory();
-
+  const history = useHistory();
+  const [notes, setNotes] = useState([])
+  
   const historyViewNotes = () => {
-    historyCards.push('/CardEdit')
-
+    history.push('/CardEdit')
   }
 
-  
+  const gNotes = async () => {
+    db.collection('newNotes').onSnapshot((querySnapshot) => {
+      const docs = [];
+      querySnapshot.forEach(doc => {
+        docs.push({ ...doc.data(), id: doc.id });
+      });
+      setNotes(docs);
+    });
+  };
+
+  const deleteNote = (id) => {
+    db.collection('newNotes').doc(id).delete()
+  }
+
+   
+  useEffect(() => {
+    gNotes();
+  }, []);
+
   return (
     <div className="pageNotes">
       <header className="headerW">
         <h1>UglyDucklingNotes</h1>
       </header>
       <div className="renderNotes">
+        {/* renderizado condicional */}
+        { notes.length > 0 ? notes.map(nota => (
+          <div className="noti" key={nota.id}>
+            <div className="noti-container" onClick={() => deleteNote(nota.id)}>
+            </div>
+          </div>
+        )) : <p>No existen notas</p>}
       </div>
       <div className="btnsNotes">
         <div className="actionsNotes">
@@ -31,8 +57,6 @@ function Cards() {
         </footer>
       </div>
     </div>
-    
-     
   )
 }
 //esta pagina contiene el render de todas las notas
